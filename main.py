b3a22f548f2db6edcc607cab40580cfd3cffe1b4
zevans4 <166666102+zevans4@users.noreply.github.com>
2024-04-11 16:34:37 -0400

    main.py
    Removed the defined encode_password(), given that it is its own file.

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ def main():
 
         option = int(input("Please enter an option: "))
 
-        def encode_password(password):
-            password_list = [int(i) for i in str(password)]
-            encoded_password_list = [str(num+3) for num in password_list]
-            encoded_password = ''.join(encoded_password_list)
-            return encoded_password
-
         def decode_password(encoded_password):
             pass
 
