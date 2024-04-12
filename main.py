58efad4c922a005e1e4f5081294fd2b460b3a76e
zevans4 <166666102+zevans4@users.noreply.github.com>
2024-04-12 17:30:11 -0400

    Update main.py
    Forgot to add %10 to my own code to check for rap-around type stuff. Oops. Should be everything fixed now, though.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
 
         def encode_password(password):
             password_list = [int(i) for i in str(password)]
-            encoded_password_list = [str(num+3) for num in password_list]
+            encoded_password_list = [str((num+3)%10) for num in password_list]
             encoded_password = ''.join(encoded_password_list)
             return encoded_password
         
