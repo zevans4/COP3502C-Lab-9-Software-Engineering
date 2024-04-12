5aa8632dfe5c3e2000f93ee90b8e6abfac29dc6e
zevans4 <166666102+zevans4@users.noreply.github.com>
2024-04-12 17:31:22 -0400

    Delete encode_password.py
    I thought it needed to be a separate .py file for some reason. Deleted now for consistency.

--- encode_password.py
+++
@@ -1,5 +0,0 @@
-def encode_password(password):
-    password_list = [int(i) for i in str(password)]
-    encoded_password_list = [str(num+3) for num in password_list]
-    encoded_password = ''.join(encoded_password_list)
-    return encoded_password
